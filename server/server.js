3ee13ccd9cec613723e51700276effebdeae008d
刘洋 <344122198@qq.com>
2018-11-26 23:35:43 +0800

    .

--- server/server.js
+++ server/server.js
@@ -3,33 +3,42 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
 
   /******** 开始 ************/
 
-  console.log('含查询字符串的路径\n' + pathWithQuery)
-
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('成功')
+  console.log('HTTP 路径为\n' + path)
+  if (path == '/style.js') {
+    response.setHeader('Content-Type', 'text/css; charset=utf-8')
+    response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
-  }else{
+  } else if (path == '/script.html') {
+    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+    response.write('alert("这是JS执行的")')
+    response.end()
+  } else if (path == '/index.css') {
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write('<!DOCTYPE>\n<html>' +
+      '<head><link rel="stylesheet" href="/style.js">' +
+      '</head><body>' +
+      '<h1>你好</h1>' +
+      '<script src="/script.html"></script>' +
+      '</body></html>')
+    response.end()
+  } else {
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('失败')
     response.end()
   }
 
